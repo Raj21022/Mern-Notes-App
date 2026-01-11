be7f051b54ffaf1ed21857dfe8f1aa2561e4541b
Raj Gogawale <rajgogawale44@gmail.com>
2026-01-11 17:50:25 +0530

    Prepared for the deployment

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,9 @@
 import axios from "axios";
-
+//in production there is no need to specify the localhost url
+const BASE_URL = import.meta.env.MODE==="deployment" ? 'http://localhost:5000/api':"/api";
 const api = axios.create({
-    baseURL: 'http://localhost:5000/api',   
+    baseURL: BASE_URL,   
 });
+
+
 export default api;
